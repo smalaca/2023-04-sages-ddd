c4630b40558d53bba3aab078fb94aec3edda66bd
smalaca <ab13.krakow@gmail.com>
2023-04-14 11:59:37 +0200

    Accept products - introduce empty CartUseCases

---
+++ src/main/java/com/smalaca/cart/adapters/rest/cart/RestCartController.java
@@ -0,0 +1,17 @@
+package com.smalaca.cart.adapters.rest.cart;
+
+import com.smalaca.cart.command.usecases.cart.CartUseCases;
+
+import java.util.UUID;
+
+public class RestCartController {
+    private final CartUseCases cartUseCases;
+
+    public RestCartController(CartUseCases cartUseCases) {
+        this.cartUseCases = cartUseCases;
+    }
+
+    public UUID acceptProducts() {
+        return cartUseCases.acceptProducts();
+    }
+}
---
+++ src/main/java/com/smalaca/cart/command/usecases/cart/CartUseCases.java
@@ -0,0 +1,9 @@
+package com.smalaca.cart.command.usecases.cart;
+
+import java.util.UUID;
+
+public class CartUseCases {
+    public UUID acceptProducts() {
+        return null;
+    }
+}
